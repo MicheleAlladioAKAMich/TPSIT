9a5ef45ee1a7081fd594d29987bff8b7502a22d5
Michele Alladio <61046970+MicheleAlladioAKAMich@users.noreply.github.com>
2021-03-24 12:41:34 +0100

    Add files via upload

---
+++ cicloPuntatori/cicloPuntatori.c
@@ -0,0 +1,23 @@
+/*
+Author: Michele Alladio
+Date: 08/10/20
+es:
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+
+int main(){
+    int *pi;
+    int vet[5] = {0,1,2,3,4};
+
+    for(pi = vet; pi - vet < 5; pi++){
+        printf("INDIRIZZO: %p\n", pi);
+        printf("VALORE DELLA CELLA: %d\n", *pi);
+        printf("POSIZIONE NEL VETTORE: %d\n", pi - vet);
+        printf("============================\n");
+    }
+}
